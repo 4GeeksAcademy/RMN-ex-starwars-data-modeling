16795a1530d3bff3f2249abcb9a1df6763afe77c
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-05-26 18:18:40 +0000

    diagrama creado

--- src/models.py
+++ src/models.py
@@ -7,23 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(30), nullable=False)
+    edad =  Column(Integer)
+    surnname = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(50), nullable=False)
+    favorite_pjs = Column(Integer, ForeignKey("fav_persons.id"))
+    favorite_planets = Column(Integer, ForeignKey("fav_planets.id"))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personajes(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    height = Column(Integer)
+    weigth = Column(Integer)
+    gender = Column(String)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    diameter = Column(Integer)
+    gravity = Column(Integer)
+    population = Column(Integer)
+    climate = Column(String)
+
+
+class Fav_personajes(Base):
+    __tablename__ = 'fav_personajes'
+    id = Column(Integer, primary_key=True)
+    favoritos = Column(Integer, ForeignKey("personajes.id"))
+
+class Fav_planets(Base):
+    __tablename__ = 'fav_planets'
+    id = Column(Integer, primary_key=True)
+    favoritos = Column(Integer, ForeignKey("planets.id"))
+
+
+
+
+
 
     def to_dict(self):
         return {}
